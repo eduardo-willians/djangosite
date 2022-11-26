4f81d6436893150de9e6e310250aa42479975777
Eduardo Willians <78479953+eduardo-willians@users.noreply.github.com>
2022-11-26 17:04:39 -0400

    wip

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,7 @@
-# from django.contrib import admin
-from django.shortcuts import render
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
