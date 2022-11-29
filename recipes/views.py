6cb71a0a724811ef932de4cb0bb353d8464459eb
Eduardo Willians <78479953+eduardo-willians@users.noreply.github.com>
2022-11-29 18:47:31 -0400

    bla

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'name': 'Eduardo',
     })
